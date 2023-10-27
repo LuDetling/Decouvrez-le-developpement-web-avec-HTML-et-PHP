d06366ab1f023ca8e89dba1a8fbdd9e5e8bd1e21
Lucas Detling <lucas.detling@gmail.com>
2023-10-27 16:13:57 +0200

    projets fini

--- Premiers_pas_PHP/index.php
+++ Premiers_pas_PHP/index.php
@@ -1,14 +1,3 @@
-<!doctype html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>The ArtBox</title>
-</head>
-
 <body>
     <?php include "header.php" ?>
     <main>
@@ -18,7 +7,7 @@
             for ($i = 0; $i < count($oeuvres); $i++) {
                 echo '
                 <article class="oeuvre">
-                <a href="oeuvre-' . $oeuvres[$i]['id'] . '.php">
+                <a href="oeuvre.php?id=' . $oeuvres[$i]['id'] . '">
                     <img src="' . $oeuvres[$i]['srcImg'] . '" alt="' . $oeuvres[$i]['title'] . '">
                     <h2>' . $oeuvres[$i]['title'] . '</h2>
                     <p class="description">' . $oeuvres[$i]['description'] . '</p>
@@ -33,5 +22,3 @@
     include "footer.php";
     ?>
 </body>
-
-</html>
--- header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
-    <nav>
-        <ul>
-            <li><a href="index.html">Accueil</a></li>
-        </ul>
-    </nav>
-</header>
