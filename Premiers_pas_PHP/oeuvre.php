587202709741661d21eb99f67b60e8fa00176ae4
Lucas Detling <lucas.detling@gmail.com>
2023-11-05 03:16:33 +0100

    projet fini

--- Premiers_pas_PHP/oeuvre.php
+++ Premiers_pas_PHP/oeuvre.php
@@ -1,24 +1,29 @@
  <?php include "header.php";
     include "oeuvres.php";
     $id = $_GET['id'];
-    foreach ($oeuvres as $oeuvre) {
-        if ($oeuvre['id'] == $id) {
-            echo '
-            <main>
-                <article id="detail-oeuvre">
-                    <div id="img-oeuvre">
-                        <img src="' . $oeuvre['srcImg'] . '" alt="' . $oeuvre['title'] . '">
-                    </div>
-                    <div id="contenu-oeuvre">
-                        <h1>' . $oeuvre['title'] . '</h1>
-                        <p class="description">' . $oeuvre['descrpition'] . '</p>
-                        <p class="description-complete">
-                        ' . $oeuvre['completeDescription'] . '
-                        </p>
-                    </div>
-                </article>
-            </main>';
+
+    $oeuvre = null;
+
+    foreach ($oeuvres as $o) {
+        if ($o['id'] === intval($id)) {
+            $oeuvre = $o;
+            break;
         }
     }
 
+    echo '
+        <main>
+            <article id="detail-oeuvre">
+                <div id="img-oeuvre">
+                    <img src="' . $oeuvre['srcImg'] . '" alt="' . $oeuvre['title'] . '">
+                </div>
+                <div id="contenu-oeuvre">
+                    <h1>' . $oeuvre['title'] . '</h1>
+                    <p class="description">' . $oeuvre['descrpition'] . '</p>
+                    <p class="description-complete">
+                    ' . $oeuvre['completeDescription'] . '
+                    </p>
+                </div>
+            </article>
+        </main>';
     include "footer.php"; ?>
